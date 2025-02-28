99cddcdf45923df0c0ab0f45d2f871783a13b13a
SLuvizotto <luvizottodev@gmail.com>
2025-02-27 23:07:21 -0300

    new routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:work_up/view/home_screen.dart';
 import '../view/login_screen.dart';
 import '../view/register_screen.dart';
 
 class AppRouter {
   static const String loginRoute = '/';
   static const String registerRoute = '/register';
+  static const String home = '/home';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -12,6 +14,8 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => LoginScreen());
       case registerRoute:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
+      case home:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
       default:
         throw Exception('Rota não encontrada: ${settings.name}');
     }
